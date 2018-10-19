d8ac5dd2fff74a45be3199ac80ae3077d7e780a3
nkprog3 <36342020+nkprog3@users.noreply.github.com>
2018-10-19 19:30:06 +0300

    Add files via upload

--- LetterGrades.py
+++ LetterGrades.py
@@ -1,17 +1,18 @@
-pointspossible=37
-
-studentname=input("Please insert your name: ")
-score=int(input("Please insert your score"))
+pointspossible=100
+#score=20
+#studentname="Nick"
+studentname=input("Please inser your name: ")
+score=int(input("Please insert your score: "))
 grade=score/pointspossible
 #print(grade)
 letter='ERROR'
 if 1>=grade>=0.9:
   letter='A'
-elif 0.9<grade<=0.8:
+elif 0.9>grade>=0.8:
   letter='B'
-elif 0.8<grade<=0.7:
+elif 0.8>grade>=0.7:
   letter='C'
-elif 0.7<grade<=0.6:
+elif 0.7>grade>=0.6:
   letter='D'
 else:
     letter='F'
